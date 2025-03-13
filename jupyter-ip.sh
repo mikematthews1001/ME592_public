abdd299a5212a1364a687321a94c64b274b4e1b9
mikematthews1001 <mikematthews2010@gmail.com>
2025-03-12 19:05:47 -0700

    Add files via upload

---
+++ jupyter-ip.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+  
+JUPYTER=$(which jupyter)
+
+if [ -z "$JUPYTER" ]; then
+   echo " "
+   echo "ERROR: Jupyter was not found. Exiting."
+   echo "Please load the Anaconda module: "
+   echo " "
+   echo "module load anaconda"
+   echo " "
+   exit
+fi
+
+if [ -z "$JS2_PUBLIC_IP" ]; then
+JS2_PUBLIC_IP=$(curl -s http://169.254.169.254/latest/meta-data/public-ipv4)
+fi
+
+#echo "JS2 IP is: $JS2_PUBLIC_IP"
+
+$JUPYTER lab --no-browser --ip=0.0.0.0 2>&1 | sed s/127.0.0.1/${JS2_PUBLIC_IP}/g
